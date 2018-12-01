21c3a5cc3ebc24eb982ec100ca932f3ec88278a6
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-12-01 01:25:02 -0300

    Add filters to market-list and change color to nav-bar

--- app/market-list/market-list.component.js
+++ app/market-list/market-list.component.js
@@ -6,11 +6,12 @@ angular.
     component('marketList', {
         templateUrl: 'market-list/market-list.template.html',
         controller: ['$http', function MarketListController($http) {
-            this.orderProp = '-Volume';
+            this.orderProp = '-Volume';            
 
             var self = this;
             $http.get("https://bittrex.com/api/v1.1/public/getmarketsummaries").then(function(response) {
                 self.markets = response.data.result;
+                self.limit = self.markets.length;
             });
         }]
   });
